587cbac14362fee2859cd8707cd24c3fe77f15e6
Michael Batrakov <mykhailobatrakov@gmail.com>
2021-02-09 21:14:41 -0500

    Create functionality to ask and answer

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,3 +1,15 @@
+# frozen_string_literal: true
+
 class AnswersController < ApplicationController
-  def answer; end
+  def answer
+    @user_question = params[:question]
+    @answer = if @user_question.include? '?'
+                'Silly question, get dressed and go to work!'
+              elsif @user_question == 'I am going to work right now!'.upcase ||
+                    @user_question == 'I am going to work right now!'.downcase
+                ''
+              else
+                "I don't care, get dressed and go to work!"
+              end
+  end
 end
--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 class QuestionsController < ApplicationController
-  def ask; end
+  def ask
+    @user_question = params[:question]
+  end
 end
